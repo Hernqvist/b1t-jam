ed30049ca66dd520c86f05d1371dd842b6e0656b
Samuel Karabetian <samuel.karabetian@gmail.com>
2025-04-05 22:57:47 +0200

    Added credits to starting text and increased viruses

--- game.gd
+++ game.gd
@@ -48,7 +48,7 @@ func on_transition() -> void:
 				return
 			system.clear_blood()
 			var tween := create_tween()
-			system.add_viruses(7)
+			system.add_viruses(8)
 			tween.tween_interval(1.2)
 			tween.tween_callback(set_state.bind(State.PLAYING))
 			virus_spawn_sound.play()
